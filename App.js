14af37c62e9d237de71bd28eec20bd16d8f81383
adnan <m_adnan_93@yahoo.com>
2020-01-11 12:47:42 +0500

    redux

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import AppNavigator from './src/navigation/index';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 export default function App() {
   return (
-      <AppNavigator />
+    <AppNavigator />
   );
 }
 
